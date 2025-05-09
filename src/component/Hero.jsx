e1180255e20fb75ac9ebaf0873e8e37309f35797
Arman_one <baseerali958@gmail.com>
2025-05-09 15:43:34 +0500

    Added 1 with anything

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       </div>
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl text-center">
-           Baseer Rent Hub, Anything
+           Baseer Rent Hub, Anything 1
         </h1>
         <p className="mt-6 max-w-lg mx-auto text-xl text-white text-center">
           Find the perfect items for your next project or adventure
